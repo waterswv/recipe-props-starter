a0b58acac40985f25f8a82652b1a7dba08f3bd1c
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-12-05 15:05:44 -0800

    updating recipe-app.js to have state from static props

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -5,25 +5,33 @@ import './RecipeApp.css';
 import Nav from './Nav';
 
 class RecipeApp extends Component {
-  static defaultProps = {
-    recipes : [
-      {
-        title: "pasta",
-        ingredients: ['flour', 'water'],
-        instructions: "Mix ingredients",
-        img: 'spaghetti.jpg'
-      },
-      {
-        title: "Avocado Toast",
-        ingredients: ['avocado', 'toast'],
-        instructions: "Toast bread, mash avocado, and spread mash over toast",
-        img: 'spaghetti.jpg'
-      }
-    ]
-  }
-
-  static propTypes = {
-    recipes: PropTypes.arrayOf(PropTypes.object)
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: [
+        {
+          id: 0,
+          title: "Spaghetti",
+          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "spaghetti.jpg"
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk.  Blend until creamy",
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img: "milkshake.jpg"
+        },
+        {
+          id: 2,
+          title: "Avocado Toast",
+          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+          img: "avocado_toast.jpg"
+        }
+      ]
+    }
   }
   render() {
     const recipes = this.props.recipes.map((r, index) => (
